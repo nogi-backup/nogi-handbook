8870e0cc0ea062e5e9db666dba6bc95a48e05bc0
payt0nc <payton@cooomma.info>
2024-03-11 01:37:31 +0800

    update: doscusaurus.config for removing default gitpage branch

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'nogi-backup', // Usually your GitHub org/user name.
   projectName: 'nogi-handbook', // Usually your repo name.
-  deploymentBranch: 'main',
+
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
