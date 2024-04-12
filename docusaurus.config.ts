8e27049f5c7bf000daf4ca0365dec45dcf440d27
payt0nc <payton@cooomma.info>
2024-04-12 23:57:17 +0800

    update: mdx import and package

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -85,7 +85,7 @@ const config: Config = {
           items: [
             {
               label: 'Calls',
-              to: '/docs/call',
+              to: '/docs/category/call',
             },
           ],
         },
---
+++ src/theme/MDXComponents.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+// Import the original mapper
+import MDXComponents from '@theme-original/MDXComponents';
+import CallHighlight from '@site/src/components/Highlights/callHighlight';
+
+export default {
+    ...MDXComponents,
+    CallHighlight,
+};
